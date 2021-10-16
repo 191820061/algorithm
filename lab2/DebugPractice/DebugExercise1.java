f9819fc5a26739547418faaa5f9fd8f2b63adde0
kongbai <191820061@smail.nju.edu.cn>
2021-10-16 14:42:26 +0800

    debug practice

--- lab2/DebugPractice/DebugExercise1.java
+++ lab2/DebugPractice/DebugExercise1.java
@@ -3,9 +3,9 @@
  * feature work.
  */
 public class DebugExercise1 {
-    public static int divideThenRound(int top, int bottom) {
-        int quotient = top / bottom;
-        int result = Math.round(quotient);
+    public static int divideThenRound(float top, float bottom) {
+        float quotient = top / bottom;
+        int result = Math.round(top / bottom);
         return result;
     }
 
